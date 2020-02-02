a6ee02854e544c536ff281c6777b043f60dc28fa
Gustav Gränsbo <gusgr644@student.liu.se>
2020-02-02 14:37:04 +0100

    Add benchmark for heap sort

---
+++ benches/heap_sort.rs
@@ -0,0 +1,63 @@
+use criterion::{criterion_group, criterion_main, Criterion, BatchSize};
+use algorithms::heap_sort::heap_sort;
+
+
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+
+fn read_test_data(path: &str) -> Result<Vec<i32>, Error> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let mut v = Vec::<i32>::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let n = line
+            .trim()
+            .parse()
+            .map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+        v.push(n);
+    }
+
+    Ok(v)
+}
+
+fn big_input_one_million(c: &mut Criterion){
+    let data = read_test_data("benches/data/sorting_1M.in").unwrap();
+    c.bench_function("Heapsort - Big Input 1M", 
+        |b| b.iter_batched_ref(
+            || data.clone(),
+            |mut data| heap_sort(&mut data),
+            BatchSize::LargeInput
+        )
+    );
+}
+
+fn big_input_ten_thousand(c: &mut Criterion){
+    let data = read_test_data("benches/data/sorting_10K.in").unwrap();
+    c.bench_function("Heapsort - Big Input 10K", 
+        |b| b.iter_batched_ref(
+            || data.clone(),
+            |mut data| heap_sort(&mut data),
+            BatchSize::SmallInput
+        )
+    );
+}
+
+fn big_input_ten_thousand_sorted(c: &mut Criterion){
+    let data = read_test_data("benches/data/sorting_sorted_10K.in").unwrap();
+    c.bench_function("Heapsort - Big Input 10K Sorted", 
+        |b| b.iter_batched_ref(
+            || data.clone(),
+            |mut data| heap_sort(&mut data),
+            BatchSize::SmallInput
+        )
+    );
+}
+
+criterion_group!{
+    name=benches;
+    config = Criterion::default().sample_size(30);
+    targets = big_input_ten_thousand, big_input_one_million, big_input_ten_thousand_sorted
+}
+criterion_main!(benches);
