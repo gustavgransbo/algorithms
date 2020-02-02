eb12f9f7537a5ed979d818f3046b85c81b7e217f
Gustav Gränsbo <gusgr644@student.liu.se>
2020-02-02 14:36:43 +0100

    Implement heap sort

---
+++ src/heap_sort.rs
@@ -0,0 +1,86 @@
+struct InPlaceHeap<'a> {
+    elements: &'a mut [i32],
+    heap_size: usize,
+}
+
+impl InPlaceHeap<'_> {
+    pub fn heapify(&mut self, i: usize) {
+        let l = InPlaceHeap::left(i);
+        let r = InPlaceHeap::right(i);
+        let mut largets_element = i;
+        if l < self.heap_size && self.elements[l] > self.elements[i] {
+            largets_element = l;
+        }
+        if r < self.heap_size && self.elements[r] > self.elements[largets_element] {
+            largets_element = r;
+        }
+        if largets_element != i {
+            self.elements.swap(i, largets_element);
+            self.heapify(largets_element);
+        }
+    }
+
+    pub fn heap_sort(&mut self) {
+        while self.heap_size > 0 {
+            self.heap_size -= 1;
+            self.elements.swap(0, self.heap_size);
+            self.heapify(0);
+        }
+    }
+
+    pub fn build_heap<'a>(vector: &'a mut [i32]) -> InPlaceHeap<'a> {
+        let size = vector.len();
+        let mut heap = InPlaceHeap {elements: vector, heap_size: size};
+        for i in (0..size/2).rev() {
+            heap.heapify(i);
+        }
+        heap
+    }
+
+    fn left(i: usize) -> usize {
+        2 * i + 1
+    }
+
+    fn right(i: usize) -> usize {
+        2 * (i + 1) 
+    }
+}
+
+pub fn heap_sort(vector: &mut [i32]){
+
+    let mut heap = InPlaceHeap::build_heap(vector);
+    heap.heap_sort();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let mut v = vec![];
+        heap_sort(&mut v);
+        assert_eq!(v, vec![]);
+    }
+
+    #[test]
+    fn small() {
+        let mut v = vec![6, 5, 4, 3, 2, 1];
+        heap_sort(&mut v);
+        assert_eq!(v, vec![1, 2, 3, 4, 5, 6]);
+    }
+
+    #[test]
+    fn sorted() {
+        let mut v = vec![1, 2, 3, 4, 5, 6];
+        heap_sort(&mut v);
+        assert_eq!(v, vec![1, 2, 3, 4, 5, 6]);
+    }
+
+    #[test]
+    fn odd_elements() {
+        let mut v = vec![3, 1, 2];
+        heap_sort(&mut v);
+        assert_eq!(v, vec![1, 2, 3]);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod merge_sort;
 pub mod quick_sort;
+pub mod heap_sort;
