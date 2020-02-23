671e4806be5b7e9142213cfda04e8ba71ad72470
Gustav Gränsbo <gusgr644@student.liu.se>
2020-02-23 12:52:58 +0100

    Make quicksort generic

--- src/quick_sort.rs
+++ src/quick_sort.rs
@@ -1,6 +1,6 @@
 use rand::{Rng, self};
 
-fn random_partition(vector: &mut [i32]) -> usize {
+fn random_partition<T: PartialOrd + Copy>(vector: &mut [T]) -> usize {
     let pivot_index = rand::thread_rng().gen_range(0, vector.len());
     vector.swap(pivot_index, vector.len() - 1);
     
@@ -8,7 +8,7 @@ fn random_partition(vector: &mut [i32]) -> usize {
 
 }
 
-fn partition(vector: &mut [i32]) -> usize {
+fn partition<T: PartialOrd + Copy>(vector: &mut [T]) -> usize {
     let pivot = vector[vector.len()-1];
     let mut i = 0;
     for j in 0..vector.len() - 1 {
@@ -23,7 +23,7 @@ fn partition(vector: &mut [i32]) -> usize {
     return i;
 }
 
-pub fn quick_sort(vector: &mut [i32]){
+pub fn quick_sort<T: PartialOrd + Copy>(vector: &mut [T]){
     if vector.len() < 2 { return; }
 
     let q = random_partition(vector);
@@ -40,7 +40,7 @@ mod tests {
 
     #[test]
     fn empy() {
-        let mut v = vec![];
+        let mut v: Vec<i32> = vec![];
         quick_sort(&mut v);
         assert_eq!(v, vec![]);
     }
@@ -64,6 +64,13 @@ mod tests {
         let mut v = vec![3, 1, 2];
         quick_sort(&mut v);
         assert_eq!(v, vec![1, 2, 3]);
+    }
+    
+    #[test]
+    fn sort_chars() {
+        let mut v = vec!['a', 'b', 'b', 'a'];
+        quick_sort(&mut v);
+        assert_eq!(v, vec!['a', 'a', 'b', 'b']);
     }
 }
 
